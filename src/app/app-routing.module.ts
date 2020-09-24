8655b8e23baac9e6a215c17772ec7cf27008d6b9
marianovalle13 <mariano@diproach.com>
2020-09-23 22:22:23 -0300

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { TechnologiesComponent } from './technologies/technologies.component';
+import { MainComponent } from 'src/shared/main/main.component';
+import { PrivacyPolicyComponent } from 'src/shared/privacy-policy/privacy-policy.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'technologies', component: TechnologiesComponent },
+  { path: 'home', component: MainComponent },
+  { path: 'privacy-plicy', component: PrivacyPolicyComponent },
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 import { TechnologiesComponent } from './technologies/technologies.component';
+import { HeaderComponent } from 'src/shared/header/header.component';
+import { NavComponent } from 'src/shared/nav/nav.component';
+import { AsideComponent } from 'src/shared/aside/aside.component';
+import { FooterComponent } from 'src/shared/footer/footer.component';
+import { PrivacyPolicyComponent } from 'src/shared/privacy-policy/privacy-policy.component';
+import { MainComponent } from 'src/shared/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent,
-    TechnologiesComponent
+    TechnologiesComponent,
+    HeaderComponent,
+    NavComponent,
+    AsideComponent,
+    FooterComponent,
+    PrivacyPolicyComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
